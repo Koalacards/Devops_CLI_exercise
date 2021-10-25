a35f96ecfe58cfe573900c9d13397ccf4937e1d8
Hayden <44925247+Koalacards@users.noreply.github.com>
2021-10-25 08:17:03 -0400

    A couple of gatekeeping changes in main.py

--- main.py
+++ main.py
@@ -7,15 +7,19 @@ def main():
 
     while True:
         command_line_input = input(f" {json_path} % ")
-
-        if command_line_input == "ls":
-            print(cli.ls())
-
         split = command_line_input.split()
-        if split[0] == "get":
+
+        if split[0] == "ls":
+            print(cli.ls())
+        elif split[0] == "get":
             #This is in case the username has any spaces in it
-            username = "".join(split[1:])
-            print(cli.get(username))
+            if len(split) > 1:
+                username = "".join(split[1:])
+                print(cli.get(username))
+            else:
+                print(f"Error: No username passed into the get function.")
+        else:
+            print(f"Command {split[0]} could not be found. The commands are 'ls' and 'get <username>'")
 
 if __name__ == "__main__":
     main()
